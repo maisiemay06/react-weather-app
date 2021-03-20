7ecb48dd7b3947e0404f42520342fea828059ed1
Maisie Palmer <maisie.palmer@outlook.com>
2021-03-20 12:31:01 +0000

    Update app and index files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Search from "./Search";
+import CurrentWeather from "./CurrentWeather";
+import MoreInfo from "./MoreInfo";
+import Forecast from "./Forecast";
+import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <Search />
+            <CurrentWeather />
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <MoreInfo />
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <Forecast />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
-
-export default App;
